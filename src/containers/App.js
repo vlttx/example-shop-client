7ef2839887edd858647e7725fa7bfd924c74612b
VBTX <jv.svedaite@gmail.com>
2019-07-14 19:34:22 -0500

    add http to fetch request

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,27 +3,6 @@ import '../App.css';
 import Items from './Items'
 
 
-const items = [
-{
-	name: "Cat Toy",
-	price: 44,
-	img_url: "https://cdn.shopify.com/s/files/1/2503/5718/products/cat-toy4.jpg?v=1540762626",
-	description: "Practical and fun"
-},
-{
-	name: "Camera",
-	price: 599,
-	img_url: "https://i5.walmartimages.com/asr/4d5bd045-d0ba-46db-bb59-39a2155bd25d_1.ecb5af3650b9cbcc5bc103144ee79d0e.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
-	description: "Take superb pictures!"
-},
-{
-	name: "Notebook",
-	price: 3.00,
-	img_url: "https://i.etsystatic.com/10467969/d/il/fdd4b9/748376030/il_340x270.748376030_goxk.jpg?version=0",
-	description: "Write about your life"
-}
-
-]
 
 class App extends Component {
 
@@ -36,7 +15,7 @@ class App extends Component {
 	}
 
 	componentDidMount(){
-		fetch('localhost:3001/api/items')
+		fetch("http://localhost:3001/api/items")
 		.then(response => response.json())
 		.then(items => this.setState({items}))
 	}
