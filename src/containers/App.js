e18ce484e7598f6975fbeb4bb096a5ad4243912c
VBTX <jv.svedaite@gmail.com>
2019-07-17 13:43:32 -0500

    add login page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import '../App.css';
-
+import Login from './components/Login'
 import Items from './Items'
 
 
 
 class App extends Component {
+	constructor(props){
+		super()
+		this.state = {
+			currentUser: null,
+		}
+	}
 
 	render() {
 		return(
 			<div className="App">
 			<Items />
+			<Login />
 			</div>
 		);
 	}
