451536374b9b8068d64aafe078ba71ed15693a62
VBTX <jv.svedaite@gmail.com>
2019-07-17 10:13:52 -0500

    add form input fields

--- src/containers/ItemForm.js
+++ src/containers/ItemForm.js
@@ -6,11 +6,43 @@ import { connect } from 'react-redux'
 
 class ItemForm extends Component {
 	render() {
+		const {name, price, img_url, description } = this.props.itemFormData
 		return (
 			<div>
 			Add an Item to the Inventory:
-
 			<form>
+			<div>
+			<label htmlFor="name"> Name:</label>
+			<input 
+				type="text"
+				name="name"
+				value={name}
+				/>
+				</div>
+				<div>
+			<label htmlFor="price"> Price:</label>
+			<input 
+				type="text"
+				name="price"
+				value={price}
+				/>
+				</div>
+				<div>
+			<label htmlFor="img_url"> Image URL:</label>
+			<input 
+				type="text"
+				name="image"
+				value={img_url}
+				/>
+				</div>
+				<div>
+			<label htmlFor="description"> Description:</label>
+			<input 
+				type="text"
+				name="description"
+				value={description}
+				/>
+				</div>
 			</form>
 			</div>
 
