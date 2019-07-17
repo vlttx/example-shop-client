364e25f1339adcab9caa44295149e30d7e16d572
VBTX <jv.svedaite@gmail.com>
2019-07-17 14:59:37 -0500

    add handleLoginFormSubmit for user authentication in the backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,12 +8,14 @@ const Login = ({handleLoginFormSubmit, handleChange, email, password}) => {
 		type="email"
 		name="email"
 		onChange={handleChange}
+		value={email}
 		/><br />
 		<input 
 		type="password"
 		name="password"
 		placeholder="password"
 		onChange={handleChange}
+		value={password}
 		/>
 		<br />
 		<input 
